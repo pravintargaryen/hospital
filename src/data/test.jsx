4dc7ff002f3ecd1f87d1d3a647bc601d5a1f52db
pravintargaryen <96950453+pravintargaryen@users.noreply.github.com>
2024-09-03 11:40:24 +0530

    Create test.jsx

---
+++ src/data/test.jsx
@@ -0,0 +1,73 @@
+import { useState, useEffect } from "react";
+import Table from "react-bootstrap/Table";
+
+
+const testFetch = () => {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://localhost:4000");
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const jsonData = await response.json();
+        console.log("Fetched data:", jsonData.messages); // Log the fetched data
+        setData(jsonData.messages);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        setError(error.message);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  if (!data) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <h1>Order Details</h1>
+      <Table className="table-danger" hover>
+        <thead>
+          <tr>
+            <th>Value</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item, index) => (
+            <tr key={index}>
+              <td>{item.message}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+      <style>{`
+        table {
+          width: 100%;
+          border-collapse: collapse;
+        }
+        th,
+        td {
+          border: 1px solid #ddd;
+          padding: 8px;
+        }
+        th {
+          background-color: #f2f2f2;
+        }
+      `}</style>
+    </div>
+  );
+};
+
+export default OrdersTable;
